3561a309eebfee8e7c852808b484ff8d9f3316d3
rajesh-danphe <54258771+rajesh-danphe@users.noreply.github.com>
2020-07-15 04:10:11 +0530

    Committing latest test cases for add update delete

--- InventoryMangement/InventoryManagement.Test/UnitTest1.cs
+++ InventoryMangement/InventoryManagement.Test/UnitTest1.cs
@@ -1,38 +1,166 @@
 using Microsoft.EntityFrameworkCore;
 using System;
 using Xunit;
+using InventoryManagement;
 using InventoryManagement.DalLayer;
+using System.Linq;
+using InventoryManagement.ServerModel;
 
 namespace InventoryManagement.Test
 {
-  public class UnitTest1
-  {
-    [Fact]
-    public void Test1()
+    public class UnitTest1
     {
-      Assert.Equal(4, this.Add(2, 2));
-    }
-    public int Add(int x, int y)
-    {
-      return x + y;
-    }
-    //[Theory(DisplayName ="Get Items List")]
-    //[InlineData("Test")]
-    public async void TestGet()
-    {
-      var options = new DbContextOptionsBuilder<InventoryDbContext>()
-                       .UseInMemoryDatabase(databaseName: "TestInventoryDB").Options;
+        [Fact(Skip = "Currently Not Required")]
+        public void Test1()
+        {
+            Assert.Equal(4, this.Add(2, 2));
+        }
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        [Fact]
+        public async void TestGet()
+        {
+            var options = new DbContextOptionsBuilder<InventoryDbContext>()
+                             .UseSqlServer("Data Source=DESKTOP-LP8KTQK\\SQLEXPRESS;Initial Catalog=InventoryManagement;Integrated Security=True").Options;
 
-      using(var context = new InventoryDbContext(options))
-      {
-        //var r1 = new InventoryList
-        //{
-        //  Name = "Rajesh"
-        //};
-      }
+            using (var context = new InventoryDbContext(options))
+            {
+
+                var test = new Inventory_Repository(context);
+                var result = await test.GetAll();
+                Assert.NotEmpty(result);
+                Assert.NotNull(result);
+            }
+
+        }
+
+        [Theory(DisplayName = "Get Items by Id")]
+        [InlineData(2)]
+        public async void TestGetById(int ItemId)
+        {
+            var options = new DbContextOptionsBuilder<InventoryDbContext>()
+                             .UseSqlServer("Data Source=DESKTOP-LP8KTQK\\SQLEXPRESS;Initial Catalog=InventoryManagement;Integrated Security=True").Options;
+
+            using (var context = new InventoryDbContext(options))
+            {
+
+                var test = new Inventory_Repository(context);
+                var result = await test.GetById(ItemId);
+                Assert.NotNull(result);
+                Assert.Contains("sasd", result.ItemName);
+
+            }
+
+        }
+        [Theory(DisplayName = "Get All Items by Id")]
+        [InlineData(2)]
+        public async void TestGetAllById(int ItemId)
+        {
+            var options = new DbContextOptionsBuilder<InventoryDbContext>()
+                             .UseSqlServer("Data Source=DESKTOP-LP8KTQK\\SQLEXPRESS;Initial Catalog=InventoryManagement;Integrated Security=True").Options;
+
+            using (var context = new InventoryDbContext(options))
+            {
+
+                var test = new Inventory_Repository(context);
+                var result = await test.GetAllById(ItemId);
+                Assert.NotNull(result);
+                Assert.Equal(1, result.Count());
+
+            }
+
+        }
+        //Skip = "Already Add is Tested",
+        [Theory( DisplayName = "Add new Item in Inventory")]
+        [InlineData("Sharpner")]
+        public async void TestAdd(string expectedName)
+        {
+            var options = new DbContextOptionsBuilder<InventoryDbContext>()
+                             .UseSqlServer("Data Source=DESKTOP-LP8KTQK\\SQLEXPRESS;Initial Catalog=InventoryManagement;Integrated Security=True").Options;
+            InventoryItemModel inventoryItem = new InventoryItemModel();
+            using (var context = new InventoryDbContext(options))
+            {
+
+
+                inventoryItem.ItemName = "Sharpener";
+                inventoryItem.ItemPrice = 10;
+                inventoryItem.ItemDescription = "Only 2 piece is remaining";
+                inventoryItem.CreatedDate = DateTime.Now;
+                var test = new Inventory_Repository(context);
+                await test.Add(inventoryItem);
+
+
+            }
+
+            using (var context = new InventoryDbContext(options))
+            {
+
+                var test = new Inventory_Repository(context);
+                var result = await test.GetById(inventoryItem.ItemId);
+                Assert.NotNull(result);
+                Assert.Contains(expectedName, result.ItemName);
+
+            }
+
+        }
+        [Theory(Skip = "Already Update is Tested", DisplayName = "Update Item in Inventory")]
+        [InlineData("Brush")]
+        public async void TestUpdate(string expectedName)
+        {
+            var options = new DbContextOptionsBuilder<InventoryDbContext>()
+                             .UseSqlServer("Data Source=DESKTOP-LP8KTQK\\SQLEXPRESS;Initial Catalog=InventoryManagement;Integrated Security=True").Options;
+            InventoryItemModel inventoryItem = new InventoryItemModel();
+            using (var context = new InventoryDbContext(options))
+            {
+
+                inventoryItem.ItemId = 12;
+                inventoryItem.ItemName = "Brush";
+                inventoryItem.ItemPrice = 12;
+                inventoryItem.ItemDescription = "Only 7 piece is remaining";
+                inventoryItem.LastUpdateDate = DateTime.Now;
+                var test = new Inventory_Repository(context);
+                await test.Update(inventoryItem);
+            }
+
+            using (var context = new InventoryDbContext(options))
+            {
+                var test = new Inventory_Repository(context);
+                var result = await test.GetById(inventoryItem.ItemId);
+                Assert.NotNull(result);
+                Assert.Contains(expectedName, result.ItemName);
+            }
+
+        }
+        [Fact]
+        public async void TestDelete()
+        {
+            var options = new DbContextOptionsBuilder<InventoryDbContext>()
+                             .UseSqlServer("Data Source=DESKTOP-LP8KTQK\\SQLEXPRESS;Initial Catalog=InventoryManagement;Integrated Security=True").Options;
+            InventoryItemModel inventoryItem = new InventoryItemModel();
+            using (var context = new InventoryDbContext(options))
+            {
+                inventoryItem.ItemId = 12;
+                inventoryItem.ItemName = "Brush";
+                inventoryItem.ItemPrice = 12;
+                inventoryItem.ItemDescription = "Only 7 piece is remaining";
+                inventoryItem.CreatedDate = DateTime.Now;
+                var test = new Inventory_Repository(context);
+                await test.Delete(inventoryItem);
+            }
+
+            using (var context = new InventoryDbContext(options))
+            {
+
+                var test = new Inventory_Repository(context);
+                var result = await test.GetById(inventoryItem.ItemId);
+                Assert.Null(result);
+            }
+
+        }
+
+
 
     }
-
- 
-  }
 }
